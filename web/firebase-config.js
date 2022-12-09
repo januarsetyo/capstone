c895f7e8a7e446262e7fff29a92a803b03b5fd4b
januarsetyo <januarsn2@gmail.com>
2022-12-09 13:50:56 +0700

    Live Chat commit

---
+++ web/firebase-config.js
@@ -0,0 +1,11 @@
+var const firebaseConfig = {
+  apiKey: "AIzaSyBJ60iUmzZEa8gi_Jtzw5xG3aLtT4jxPXI",
+  authDomain: "capstone-c22-117.firebaseapp.com",
+  databaseURL: "https://capstone-c22-117-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "capstone-c22-117",
+  storageBucket: "capstone-c22-117.appspot.com",
+  messagingSenderId: "948516375676",
+  appId: "1:948516375676:web:9be374dcac7965e8194df9",
+  measurementId: "G-XJZ86VN0LN"
+};
+firebase.initializeApp(firebaseConfig);
